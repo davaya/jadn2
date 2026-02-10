a24599a1f32fd10f40b310e038031077df7888bd
David Kemp <19152940+davaya@users.noreply.github.com>
2026-02-10 08:14:33 -0500

    refactor convert tests

--- apps/zclass.py
+++ apps/zclass.py
@@ -1,25 +1,31 @@
-from typing import Self
+import copy
 
 class Base:
-    def __init__(self, x: Base=None) -> None:
-        self.value = x.value if x is not None else None
+    def __init__(self, obj: 'Base'=None) -> None:
+        self.x = None
+        self.y = None
+        if obj is not None:
+            self.__dict__.update(obj.__dict__)
 
     def load_value(self):
         raise NotImplementedError
 
 class A(Base):
     def load_value(self):
-        self.value = 'aaa'
+        self.x = 'aaa'
+        self.y = 'a1'
 
 class B(Base):
     def load_value(self):
-        self.value = 'bbb'
+        self.x = 'bbb'
+        self.y = 'b2'
 
 
 if __name__ == '__main__':
     objA = A()
-    print('A0:', objA, objA.value)
+    print('A0:', objA, objA.x, objA.y)
     objA.load_value()
-    print('A1:', objA, objA.value)
+    print('A1:', objA, objA.x, objA.y)
     objB = B(objA)
-    print(' B:', objB, objB.value)
+    print(' B:', objB, objB.x, objB.y)
+    print(objB.__dict__)
