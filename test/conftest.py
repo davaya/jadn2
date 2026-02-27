cad52b550c184387e9f004194c93f6f6599c1ac2
David Kemp <19152940+davaya@users.noreply.github.com>
2026-02-27 08:23:45 -0500

    refactor convert tests

--- test/conftest.py
+++ test/conftest.py
@@ -4,7 +4,8 @@ import pytest
 OUT_DIR = 'Out'
 
 @pytest.fixture(scope='session')
-def overall_setup():
+def session_data():
     print('Performing overall setup')
     os.makedirs(OUT_DIR, exist_ok=True)
-    yield OUT_DIR
+    yield {'output_dir': OUT_DIR}
+    print('Performing overall teardown')
--- test/test_jadn_convert.py
+++ test/test_jadn_convert.py
@@ -8,7 +8,6 @@ from jadn.config import style_args, style_fname
 
 JADN_SCHEMA_DIR = 'apps/schemas/jadn'
 CONFIG_FILE = 'apps/jadn_config.json'
-OUT_DIR = 'Out'
 JADN_SCHEMA_CLASS = {
     'jadn': JADN,
     'jidl': JIDL,
@@ -29,37 +28,40 @@ def get_jadn_schemas():
 @pytest.mark.parametrize('test', ['dump', 'load', 'round_trip'])
 @pytest.mark.parametrize('schema_format', JADN_SCHEMA_CLASS)
 @pytest.mark.parametrize('schema_path', get_jadn_schemas())
-def test_jadn_schema_convert(schema_path, schema_format, test):
+def test_jadn_schema_convert(session_data, schema_path, schema_format, test):
     schema_file  = os.path.split(schema_path)[1]
-    fn, ext = os.path.splitext(schema_file)
-    ext = ext.lstrip('.')
-    if ext in JADN_SCHEMA_CLASS:     # Ignore directories and non-schema files
-        if in_pkg := JADN_SCHEMA_CLASS[ext]():   # and 'schema_loads' in dir(in_pkg):  # Input format has a load method
-            with open(schema_path, 'r') as fp:
-                if ext in {'jadn', 'jidl', 'xasd', 'md', 'json'}:
-                    in_pkg.schema_load(fp)
-                    schema_convert(fn, in_pkg)
-                else:
-                    with pytest.raises(NotImplementedError):
-                        in_pkg.schema_load(fp)
+    fn = os.path.splitext(schema_file)[0]
+    if test == 'dump':  # Convert JADN schema to equivalent formats
+        in_pkg = JADN_SCHEMA_CLASS['jadn']()
+        for out_format in JADN_SCHEMA_CLASS:
+            schema_convert(fn, in_pkg, schema_path, out_format, session_data['output_dir'])
+    elif test == 'load':    # Convert equivalent schema formats to JADN
+        in_pkg = JADN_SCHEMA_CLASS[schema_format]()
+        # in_file = os.path.join(fn, )
+    elif test == 'round_trip':  # Verify lossless conversion from JADN to other format and back
+        pass
+
+    """
+    with pytest.raises(NotImplementedError):
+        in_pkg.schema_load(fp)
+    """
+
+
+def schema_convert(fn: str, in_pkg: 'JADNCore', in_path: str, out_fmt: str, out_dir: str) -> None:
+    with open(in_path, 'r', encoding='utf8') as fp:
+        in_pkg.schema_load(fp)
+    out_pkg = JADN_SCHEMA_CLASS[out_fmt](in_pkg)
+    style = style_args(out_pkg, out_fmt, '', CONFIG_FILE)  # need out_pkg
+    if out_fmt in {'jadn', 'jidl', 'xasd', 'md', 'erd', 'atree'}:
+        convert_out(fn, out_fmt, out_pkg, style, out_dir)
     else:
-        print(f'\nUnknown input format "{ext}" -- ignored')
+        with pytest.raises(NotImplementedError):
+            convert_out(fn, out_fmt, out_pkg, style, out_dir)
 
 
-def schema_convert(fn: str, in_pkg: 'JADNCore') -> None:
-    for out_format in JADN_SCHEMA_CLASS:
-        out_pkg = JADN_SCHEMA_CLASS[out_format](in_pkg)
-        style = style_args(out_pkg, out_format, '', CONFIG_FILE)  # need out_pkg
-        if out_format in {'jadn', 'jidl', 'xasd', 'md', 'erd', 'atree'}:
-            convert_out(fn, out_format, out_pkg, style)
-        else:
-            with pytest.raises(NotImplementedError):
-                convert_out(fn, out_format, out_pkg, style)
-
-
-def convert_out(fn: str, out_format: str, out_pkg: 'JADNCore', style: dict) -> None:
-    if OUT_DIR:
-        with open(os.path.join(OUT_DIR, style_fname(fn, out_format, style)), 'w', encoding='utf8') as fp:
+def convert_out(fn: str, out_format: str, out_pkg: 'JADNCore', style: dict, out_dir: str) -> None:
+    if out_dir:
+        with open(os.path.join(out_dir, style_fname(fn, out_format, style)), 'w', encoding='utf8') as fp:
             out_pkg.schema_dump(fp, style)
     else:
         out_pkg.schema_dump(sys.stdout, style)
