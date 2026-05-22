a48be52cfd78c4cacb3d171f6214f1bfb9103290
David Kemp <19152940+davaya@users.noreply.github.com>
2026-05-22 14:11:46 -0400

    json schema wellformed

--- test/test_wellformed.py
+++ test/test_wellformed.py
@@ -1,12 +1,19 @@
 import json
-
-from jadn.core import data_dir
-from jsonschema import validate
 import os
 import pytest
+from jadn.core import data_dir
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from pathlib import Path
 
 JADN_SCHEMA_DIR = 'schemas/jadn'
-JADN_BAD_SCHEMA_DIR = 'schemas/jadn-errs'
+JADN_BAD_SCHEMA_DIR = 'schemas/jadn-not-wellformed'
+
+
+def abs_dir(relative_path: str) -> Path:
+    p = Path(__file__).parts
+    parts = p[:p.index('test')] + Path(relative_path).parts
+    return Path(*parts)
 
 
 @pytest.fixture(scope='session')
@@ -18,15 +25,16 @@ def json_schema():
     # Teardown
 
 
-@pytest.mark.parametrize('in_path', os.listdir(JADN_SCHEMA_DIR))
+@pytest.mark.parametrize('in_path', Path(abs_dir(JADN_SCHEMA_DIR)).glob('*'), ids=lambda p: p.name)
 def test_jadn_wellformed(in_path: str, json_schema) -> None:
     with open(os.path.join(JADN_SCHEMA_DIR, in_path)) as f:
         jadn_schema = json.load(f)
     validate(instance=jadn_schema, schema=json_schema)
 
 
-@pytest.mark.parametrize('in_path', os.listdir(JADN_BAD_SCHEMA_DIR))
+@pytest.mark.parametrize('in_path', Path(abs_dir(JADN_BAD_SCHEMA_DIR)).glob('*'), ids=lambda p: p.name)
 def test_jadn_wellformed_errs(in_path: str, json_schema) -> None:
     with open(os.path.join(JADN_BAD_SCHEMA_DIR, in_path)) as f:
         jadn_schema = json.load(f)
-    validate(instance=jadn_schema, schema=json_schema)
+    with pytest.raises(ValidationError):
+        validate(instance=jadn_schema, schema=json_schema)
